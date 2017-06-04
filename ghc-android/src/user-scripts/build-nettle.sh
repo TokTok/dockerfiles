c56ef03a301b675b34d72179026a2ce07a6f0f44
Zoff <zoff99@users.noreply.github.com>
2017-06-04 21:02:33 +0200

    Create circle.yml

--- ghc-android/src/user-scripts/build-nettle.sh
+++ ghc-android/src/user-scripts/build-nettle.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -x
+
 . set-env.sh
-####################################################################################################
 
 cd $NDK_ADDON_SRC
+
 apt-get source nettle
+# curl 'https://ftp.gnu.org/gnu/nettle/nettle-3.3.tar.gz' --output nettlXe.tar.gz
+# tar -xzvf nettlXe.tar.gz
 
 pushd nettle*
 sed -i -e 's/__gmpz_mpz_powm/__gmpz_powm/' configure.ac

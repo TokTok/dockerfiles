5d2acb96b522e0f1f0bed97dbd98156bd563721f
iphydf <iphydf@users.noreply.github.com>
2017-06-06 23:05:56 +0000

    Patch nettle configure script to look for a different symbol.
    Apparently the symbol it's looking for isn't in gmp 6.1.1, making it
    think that the version is older than 5.0. Wonderful!

--- ghc-android/src/user-scripts/build-nettle.sh
+++ ghc-android/src/user-scripts/build-nettle.sh
@@ -7,6 +7,7 @@ cd $NDK_ADDON_SRC
 apt-get source nettle
 
 pushd nettle*
+sed -i -e 's/__gmpz_mpz_powm/__gmpz_powm/' configure.ac
 autoreconf -fi
 ./configure --prefix="$NDK_ADDON_PREFIX" --host=$NDK_TARGET --build=$BUILD_ARCH --with-build-cc=$BUILD_GCC --enable-static --disable-shared
 make $MAKEFLAGS

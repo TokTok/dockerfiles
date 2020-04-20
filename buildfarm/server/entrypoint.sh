fc700c241b41d162a3d9a5f39e396ecee0b66b76
iphydf <iphydf@users.noreply.github.com>
2020-04-20 21:51:08 +0000

    Add bazel buildfarm dockerfiles.

---
+++ buildfarm/server/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eux
+
+if [ "${1-}" = "/bin/sh" ]; then
+   exec "$@"
+fi
+
+PORT="${1-8980}"
+
+sed -i -e "s/@PORT@/$PORT/g" /config/server.config
+
+exec java -Djava.util.logging.config.file=/config/logging.properties -jar /buildfarm-server_deploy.jar /config/server.config

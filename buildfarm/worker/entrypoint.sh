ce28f13bc800a2904ea63c6e233b2fb545c1e9b1
iphydf <iphydf@users.noreply.github.com>
2021-12-02 00:55:18 +0000

    chore: Fix buildfarm and kythe docker images.
    They needed new bazel versions.

--- buildfarm/worker/entrypoint.sh
+++ buildfarm/worker/entrypoint.sh
@@ -12,4 +12,4 @@ PORT="${2-8980}"
 sed -i -e "s/@HOST@/$HOST/g" /config/worker.config
 sed -i -e "s/@PORT@/$PORT/g" /config/worker.config
 
-exec java -Djava.util.logging.config.file=/config/logging.properties -jar /buildfarm-operationqueue-worker_deploy.jar /config/worker.config
+exec java -Djava.util.logging.config.file=/config/logging.properties -jar /buildfarm-memory-worker_deploy.jar /config/worker.config

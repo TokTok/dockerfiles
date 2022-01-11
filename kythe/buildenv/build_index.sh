5baa1b7ce5ef1447b4e27c4a431781d9d481b4ac
iphydf <iphydf@users.noreply.github.com>
2022-01-11 20:45:49 +0000

    fix: Disable layering check in Kythe index build.
    Also attempt to fix the FreeBSD image using public key authentication.
    It's not working yet, but neither does what we had before.

--- kythe/buildenv/build_index.sh
+++ kythe/buildenv/build_index.sh
@@ -4,16 +4,19 @@ set -eux
 
 # Android SDK is needed even when we don't build Android stuff because WORKSPACE
 # configuration fails otherwise.
-tools/prepare_android_sdk.sh
+ln -s /third_party/android/sdk third_party/android/sdk
 
 # We list the packages to build here instead of in the "tables" image, because
 # we should never build a different set of paths in that image when building
 # from different repositories. E.g. both dockerfiles and toktok-stack want to
 # push this image.
+#
+# Also note we disable layering_check because it's incompatible with Kythe.
 bazel --bazelrc=/opt/kythe/extractors.bazelrc build \
   --build_tag_filters=-haskell \
   --test_tag_filters=-haskell \
   --config=clang \
+  --features=-layering_check \
   --override_repository kythe_release=/opt/kythe \
   //c-toxcore/... \
   //toxic/...

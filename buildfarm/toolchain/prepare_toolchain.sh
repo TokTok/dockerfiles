b8d957d372a39fca459f78628dabcfe13c11efb8
iphydf <iphydf@users.noreply.github.com>
2023-08-10 19:08:24 +0000

    chore: Add a toolchain build for bazel 6.3.2.

--- buildfarm/toolchain/prepare_toolchain.sh
+++ buildfarm/toolchain/prepare_toolchain.sh
@@ -2,13 +2,18 @@
 
 set -eux
 
-wget -qO /tmp/rbe_configs_gen https://github.com/bazelbuild/bazel-toolchains/releases/download/v5.1.1/rbe_configs_gen_linux_amd64
+BAZEL_VERSION="${1:-}"
+if [ -z "$BAZEL_VERSION" ]; then
+  BAZEL_VERSION="$(wget -qO- https://raw.githubusercontent.com/TokTok/toktok-stack/master/.bazelversion)"
+fi
+
+wget -qO /tmp/rbe_configs_gen https://github.com/bazelbuild/bazel-toolchains/releases/download/v5.1.2/rbe_configs_gen_linux_amd64
 chmod +x /tmp/rbe_configs_gen
 
 # This uses toxchat/bazel instead of toxchat/builder so it can see all the
 # installed headers for the various out-of-tree system dependencies.
 /tmp/rbe_configs_gen \
-  --bazel_version="$(wget -qO- https://raw.githubusercontent.com/TokTok/toktok-stack/master/.bazelversion)" \
+  --bazel_version="$BAZEL_VERSION" \
   --toolchain_container=toxchat/bazel:latest \
   --output_src_root="$PWD" \
   --output_config_path=toolchain \

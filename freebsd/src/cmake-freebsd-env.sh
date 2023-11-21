c0e384c73c876b406320e98b0021aa14551d5cc6
iphydf <iphydf@users.noreply.github.com>
2023-11-21 14:42:07 +0000

    feat: Add an alpine/s390x qemu container.

--- freebsd/src/cmake-freebsd-env.sh
+++ freebsd/src/cmake-freebsd-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2018-2021 nurupo
+# Copyright (C) 2018-2023 nurupo
 
 # Common variables and functions
 
@@ -8,7 +8,7 @@ NPROC="$(nproc)"
 SCREEN_SESSION=freebsd
 SSH_PORT=10022
 
-FREEBSD_VERSION="12.3"
-IMAGE_NAME="FreeBSD-$FREEBSD_VERSION-RELEASE-amd64.raw"
-# https://download.freebsd.org/ftp/releases/VM-IMAGES/12.3-RELEASE/amd64/Latest/
-IMAGE_SHA512="29c4b670bd2adbc4f8880ab74e62a1409985af1f76e2ebb160b8a9a9dfc66b741915171401471e45b4dcd59212e1e50c68685f4dbbd96e86b67e56cc26a39017"
+FREEBSD_VERSION="14.0"
+IMAGE_NAME="FreeBSD-$FREEBSD_VERSION-RELEASE-amd64.qcow2"
+# https://download.freebsd.org/ftp/releases/VM-IMAGES/14.0-RELEASE/amd64/Latest/
+IMAGE_SHA512="4fe194b45a57c601bbc9e48d6f653b461e040757383cc0d690718e9f726e3b799074aa408e10e544ca3e988a822f42d7204ec660d43533d927fa7503db0fca83"

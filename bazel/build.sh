79d7946f1724916b28f27837f3cee881abe0254e
iphydf <iphydf@users.noreply.github.com>
2024-02-04 01:18:21 +0000

    feat: Add doxygen image.
    We build from source, because we want clang parsing support.

--- bazel/build.sh
+++ bazel/build.sh
@@ -5,9 +5,11 @@ set -eux
 # Install nix if it's not there yet (e.g. on Github Actions).
 if ! which nix-shell; then
   curl -L https://nixos.org/nix/install -o install.sh
-  sh install.sh --no-daemon --yes
+  sh install.sh --no-daemon --no-channel-add --yes
   rm install.sh
   . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+  nix-channel --add https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz nixpkgs
+  nix-channel --update
 fi
 
 # Build a docker image and squash all the layers.

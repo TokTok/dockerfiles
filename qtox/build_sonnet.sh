b863af5d62c74f4f6736ae4c75d85d0a1c03dcc9
iphydf <iphydf@users.noreply.github.com>
2025-01-06 22:32:02 +0000

    fix: Link against nsspellchecker on macos-arm64.

--- qtox/build_sonnet.sh
+++ qtox/build_sonnet.sh
@@ -25,7 +25,7 @@ else
   HUNSPELL_LIBRARIES="$(echo "$DEP_PREFIX"/lib/libhunspell*.a)"
   find . -name CMakeLists.txt -exec sed -i '' -e 's/ MODULE$/ STATIC/g' '{}' ';'
   find . -name CMakeLists.txt -exec sed -i '' -e 's/install(TARGETS sonnet_\([^ ]*\) /&EXPORT KF6SonnetTargets/g' '{}' ';'
-  if [ "$SCRIPT_ARCH" == "macos-x86_64" ]; then
+  if [ "$SCRIPT_ARCH" = "macos-x86_64" ] || [ "$SCRIPT_ARCH" = "macos-arm64" ]; then
     find . -name CMakeLists.txt -exec sed -i '' -e 's/target_link_libraries(KF6SonnetCore PUBLIC Qt6::Core)/target_link_libraries(KF6SonnetCore PUBLIC Qt6::Core sonnet_hunspell sonnet_nsspellchecker)/' '{}' ';'
   else
     find . -name CMakeLists.txt -exec sed -i '' -e 's/target_link_libraries(KF6SonnetCore PUBLIC Qt6::Core)/target_link_libraries(KF6SonnetCore PUBLIC Qt6::Core sonnet_hunspell)/' '{}' ';'

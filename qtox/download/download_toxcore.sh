6a35aecc9009599f2433f0d6bbd68ad7d4687e9b
Green Sky <Green-Sky@users.noreply.github.com>
2025-09-30 15:00:08 +0200

    chore: update qtox toxcore to 0.2.21

--- qtox/download/download_toxcore.sh
+++ qtox/download/download_toxcore.sh
@@ -6,11 +6,11 @@
 
 set -euo pipefail
 
-TOXCORE_VERSION=0.2.20
-TOXCORE_HASH=a9c89a8daea745d53e5d78e7aacb99c7b4792c4400a5a69c71238f45d6164f4c
+TOXCORE_VERSION=0.2.21
+TOXCORE_HASH=3443a45d085fb3dee20514243787d06acde2d6c89130076d6f932534581a4ac7
 
 source "$(dirname "$(realpath "$0")")/common.sh"
 
 download_verify_extract_tarball \
-  https://github.com/TokTok/c-toxcore/releases/download/v"$TOXCORE_VERSION/c-toxcore-$TOXCORE_VERSION".tar.gz \
+  https://github.com/TokTok/c-toxcore/releases/download/v"$TOXCORE_VERSION/c-toxcore-v$TOXCORE_VERSION".tar.gz \
   "$TOXCORE_HASH"
